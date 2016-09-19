a64327310455698805e25a246789f3a51acd3090
David Luecke <daff@neyeon.com>
2016-09-19 10:49:51 -0700

    Add QUnit reporting for SauceLabs

--- steal-qunit.js
+++ steal-qunit.js
@@ -5,7 +5,11 @@ define([
 	"qunitjs/qunit/qunit.css!"
 ], function(loader, QUnit){
 
-	if(loader.has("live-reload")) setupLiveReload();
+	if(loader.has("live-reload")) {
+		setupLiveReload();
+	}
+
+	setupSauceLabsReporting();
 
 	function setupLiveReload(){
 		QUnit.done(updateResults);
@@ -39,6 +43,36 @@ define([
 				parent.removeChild(node);
 			}
 		}
+	}
+
+	function setupSauceLabsReporting() {
+		var log = [];
+		  
+		QUnit.done(function (test_results) {
+		  var tests = [];
+		  for(var i = 0, len = log.length; i < len; i++) {
+		    var details = log[i];
+		    tests.push({
+		      name: details.name,
+		      result: details.result,
+		      expected: details.expected,
+		      actual: details.actual,
+		      source: details.source
+		    });
+		  }
+		  test_results.tests = tests;
+		  
+		  window.global_test_results = test_results;
+		});
+
+		QUnit.testStart(function(testDetails){
+		  QUnit.log(function(details){
+		    if (!details.result) {
+		      details.name = testDetails.name;
+		      log.push(details);
+		    }
+		  });
+		});
 	}
 
 	QUnit.config.autorun = false;
